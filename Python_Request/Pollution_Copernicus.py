af98bceedcebd425950432e2d2bebc5e33ef489e
Pratt SAVI <58483678+PrattSAVI@users.noreply.github.com>
2020-09-21 12:42:33 +0300

    'Update time and date entry'

--- Python_Request/Pollution_Copernicus.py
+++ Python_Request/Pollution_Copernicus.py
@@ -2,10 +2,21 @@
 
 import cdsapi
 import netCDF4
+import xarray as xr
+import pandas as pd
+import datetime as dt
+#%% Setup variables for Query
+
+times = [str(i).zfill(2) + ':00' for i in range(0,24)] #Each hour from 00 to 23
+
+today = dt.datetime.now().strftime( '%Y-%m-%d' )
+yesterday = (dt.datetime.now()-dt.timedelta(days=1)).strftime( '%Y-%m-%d' )
+dates = yesterday + '/' + today #From yesterday to today, how to get fro tomorrow.
+
+#%% Query API
 
 c = cdsapi.Client()
-
-file_path = r'C:\Users\csucuogl\Desktop\WORK\MISC_TEST\Istanbul_AirQuality\test.nc'
+file_path = r'C:\Users\csucuogl\Desktop\WORK\MISC_TEST\Istanbul_AirQuality\test.nc' #Saving Locally For Now
 
 c.retrieve(
     'cams-europe-air-quality-forecasts',
@@ -13,13 +24,9 @@ c.retrieve(
         'variable': 'particulate_matter_2.5um',
         'model': 'ensemble',
         'level': '0',
-        'date': '2020-09-16/2020-09-18',
-        'type': 'analysis',
-        'time': [
-            '15:00', '16:00', '17:00',
-            '18:00', '19:00', '20:00',
-            '21:00', '22:00', '23:00',
-        ],
+        'date': dates ,
+        'type': 'forecast',
+        'time': times ,
         'leadtime_hour': '0',
         'area': [
             41.48, 28.17, 40.67,
@@ -28,19 +35,10 @@ c.retrieve(
         'format': 'netcdf',
     }, file_path )
 
+ds_disk = xr.open_dataset( file_path )
+dd = ds_disk.to_dict() #Convert to dict to convert to pandas
 
-#%% OPEN in Python
-
-import xarray as xr
-import pandas as pd
-
-ds_disk = xr.open_dataset( file_path)
-
-dd = ds_disk.to_dict()
-
-#%%
-
-#For first time entry
+#%% Convert 4D data to long Pandas List.
 dfo = pd.DataFrame(columns = ['lat','lon','time','values'])
 for i in range( len( dd['data_vars']['pm2p5_conc']['data'] ) ): #for each time
 
@@ -50,6 +48,9 @@ for i in range( len( dd['data_vars']['pm2p5_conc']['data'] ) ): #for each time
     
     df = df.stack().reset_index()
     df.columns = ['lat','lon','values']
+    print( dd['coords']['time']['data'][i] )
     df['time'] = dd['coords']['time']['data'][i]
     
     dfo = dfo.append( df )
+
+# %%
